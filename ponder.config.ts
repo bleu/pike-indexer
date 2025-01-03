12bdcffa0f16e8a978614f2cc2fa44a50b56bbdc
Pedro Yves Fracari <yves.fracari@gmail.com>
2025-01-03 09:21:48 -0300

    chore: add factory and risk engine addresses on config

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,30 @@
-import { createConfig } from "ponder";
+import { createConfig, mergeAbis } from "ponder";
 import { http } from "viem";
 
-import { ExampleContractAbi } from "./abis/ExampleContractAbi";
+import { ProxyAbi } from "./abis/ProxyAbi";
+import { baseSepolia } from "viem/chains";
+import { FactoryAbi } from "./abis/FactoryAbi";
+import { RiskEngineRouterAbi } from "./abis/RiskEngineRouterAbi";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
-      transport: http(process.env.PONDER_RPC_URL_1),
+    baseSepolia: {
+      chainId: baseSepolia.id,
+      transport: http(process.env.BASE_SEPOLIA_URL),
     },
   },
   contracts: {
-    ExampleContract: {
-      network: "mainnet",
-      abi: ExampleContractAbi,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+    Factory: {
+      network: "baseSepolia",
+      abi: mergeAbis([FactoryAbi, ProxyAbi]),
+      address: "0xF5b46BCB51963B8A7e0390a48C1D6E152A78174D",
+      startBlock: 19991778,
+    },
+    RiskEngineRouter: {
+      network: "baseSepolia",
+      address: "0x0cdf5f46edb9ae9655086c4e49fa7f3f9fddb298",
+      startBlock: 19988290,
+      abi: mergeAbis([ProxyAbi, RiskEngineRouterAbi]),
     },
   },
 });
