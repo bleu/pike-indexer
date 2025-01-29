453a1441a89f1947d960f3dd6bedb1c1e337ad91
Pedro Yves Fracari <yves.fracari@gmail.com>
2025-01-29 13:14:31 -0300

    docs: add ponder documentation middle ware and update indexer readme

--- packages/ponder-app/src/api/index.ts
+++ packages/ponder-app/src/api/index.ts
@@ -3,10 +3,14 @@ import schema from 'ponder:schema';
 import { db } from 'ponder:api';
 
 import { Hono } from 'hono';
+import docs from 'src/utils/docs';
+import { createDocumentationMiddleware } from 'ponder-enrich-gql-docs-middleware';
 
 const app = new Hono();
 
+app.use('/', createDocumentationMiddleware(docs));
 app.use('/', graphql({ db, schema }));
+app.use('/graphql', createDocumentationMiddleware(docs));
 app.use('/graphql', graphql({ db, schema }));
 
 export default app;
