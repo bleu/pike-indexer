1be9f025c49e0d5bbd8d1d18a7eaeef692f54584
Pedro Yves Fracari <yves.fracari@gmail.com>
2025-02-12 15:52:36 -0300

    refactor: remove boiderplate code from config

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,15 +1,33 @@
 import { createConfig, factory } from 'ponder';
-import { http, parseAbiItem } from 'viem';
-
+import { http, parseAbiItem, Address } from 'viem';
 import { arbitrumSepolia, baseSepolia, optimismSepolia } from 'viem/chains';
 import { FactoryAbi } from './abis/FactoryAbi';
 import { RiskEngineAbi } from './abis/RiskEngineAbi';
 import { PTokenAbi } from './abis/PTokenAbi';
 import { BeaconAbi } from './abis/BeaconAbi';
 
-const CHAIN_CONFIG = {
+const HOUR = 60 * 60;
+const DAY = HOUR * 24;
+
+type ChainId =
+  | typeof baseSepolia.id
+  | typeof optimismSepolia.id
+  | typeof arbitrumSepolia.id;
+
+interface ChainConfig {
+  chainId: ChainId;
+  factoryAddress: Address;
+  factoryStartBlock: number;
+  blockTime: number;
+  beaconAddresses: Address[];
+  beaconStartBlock: number;
+  rpcEnvKey: string;
+}
+
+const CHAIN_CONFIGS: Record<ChainId, ChainConfig> = {
   [baseSepolia.id]: {
-    factoryAddress: '0xF5b46BCB51963B8A7e0390a48C1D6E152A78174D',
+    chainId: baseSepolia.id,
+    factoryAddress: '0xF5b46BCB51963B8A7e0390a48C1D6E152A78174D' as Address,
     factoryStartBlock: 19991778,
     blockTime: 2,
     beaconAddresses: [
@@ -17,11 +35,13 @@ const CHAIN_CONFIG = {
       '0x419aDB9e3Aa8B89b25915689332905a04528Abf6',
       '0xdEDA2fFc4F212c41f1a54c3a6136Df0BFaEcAeEC',
       '0x8f3362a4Da07C9D7F59f9332B0F4c09Db8A89e41',
-    ],
+    ] as Address[],
     beaconStartBlock: 19989047,
-  } as const,
+    rpcEnvKey: 'BASE_SEPOLIA_RPC_URL',
+  },
   [optimismSepolia.id]: {
-    factoryAddress: '0x82072C90aacbb62dbD7A0EbAAe3b3e5D7d8cEEEA',
+    chainId: optimismSepolia.id,
+    factoryAddress: '0x82072C90aacbb62dbD7A0EbAAe3b3e5D7d8cEEEA' as Address,
     factoryStartBlock: 22061870,
     blockTime: 2,
     beaconAddresses: [
@@ -29,11 +49,13 @@ const CHAIN_CONFIG = {
       '0xfdc91BdDc40D71aC91db9B9ea3beaA939b85f4fc',
       '0x68c81Ac75689e20a18Ff00Ab9f4AAAb2d99912f7',
       '0x1Dba4Ed49f6949aAb39abA2d59211fc657546719',
-    ],
+    ] as Address[],
     beaconStartBlock: 22061839,
-  } as const,
+    rpcEnvKey: 'OPTIMISM_SEPOLIA_RPC_URL',
+  },
   [arbitrumSepolia.id]: {
-    factoryAddress: '0x82072C90aacbb62dbD7A0EbAAe3b3e5D7d8cEEEA',
+    chainId: arbitrumSepolia.id,
+    factoryAddress: '0x82072C90aacbb62dbD7A0EbAAe3b3e5D7d8cEEEA' as Address,
     factoryStartBlock: 112780355,
     blockTime: 0.2,
     beaconAddresses: [
@@ -41,194 +63,181 @@ const CHAIN_CONFIG = {
       '0xfdc91BdDc40D71aC91db9B9ea3beaA939b85f4fc',
       '0x5f45CBcDFD790e5f45D2b5B81E293aaC2EF2b622',
       '0x1Dba4Ed49f6949aAb39abA2d59211fc657546719',
-    ],
+    ] as Address[],
     beaconStartBlock: 112780101,
-  } as const,
+    rpcEnvKey: 'ARBITRUM_SEPOLIA_RPC_URL',
+  },
 };
 
-const HOUR = 60 * 60;
-const DAY = HOUR * 24;
+const createNetworkConfigs = () => {
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        chainId: parseInt(chainId),
+        transport: http(process.env[config.rpcEnvKey]),
+      },
+    };
+  }, {});
+};
+
+const createBlockConfigs = (intervalType: 'HOUR' | 'DAY') => {
+  const interval = intervalType === 'HOUR' ? HOUR : DAY;
+
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        startBlock: config.factoryStartBlock,
+        interval: Math.round(interval / config.blockTime),
+      },
+    };
+  }, {});
+};
+
+interface ContractNetworkConfig<T> {
+  address: T;
+  startBlock: number;
+}
+
+type FactoryConfig = {
+  network: Record<string, ContractNetworkConfig<Address>>;
+  abi: typeof FactoryAbi;
+};
+
+type RiskEngineConfig = {
+  network: Record<string, ContractNetworkConfig<ReturnType<typeof factory>>>;
+  abi: typeof RiskEngineAbi;
+};
+
+type PTokenConfig = {
+  network: Record<string, ContractNetworkConfig<ReturnType<typeof factory>>>;
+  abi: typeof PTokenAbi;
+};
+
+type BeaconConfig = {
+  network: Record<string, ContractNetworkConfig<Address[]>>;
+  abi: typeof BeaconAbi;
+};
+
+const createFactoryConfig = (): FactoryConfig['network'] => {
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        address: config.factoryAddress,
+        startBlock: config.factoryStartBlock,
+      },
+    };
+  }, {});
+};
+
+const createRiskEngineConfig = (): RiskEngineConfig['network'] => {
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        address: factory({
+          address: config.factoryAddress,
+          event: parseAbiItem(
+            'event ProtocolDeployed(uint256 indexed protocolId, address indexed riskEngine, address indexed timelock, address initialGovernor)'
+          ),
+          parameter: 'riskEngine',
+        }),
+        startBlock: config.factoryStartBlock,
+      },
+    };
+  }, {});
+};
+
+const createPTokenConfig = (): PTokenConfig['network'] => {
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        address: factory({
+          address: config.factoryAddress,
+          event: parseAbiItem(
+            'event PTokenDeployed(uint256 indexed protocolId, uint256 indexed index, address indexed pToken, address timelock)'
+          ),
+          parameter: 'pToken',
+        }),
+        startBlock: config.factoryStartBlock,
+      },
+    };
+  }, {});
+};
+
+const createBeaconConfig = (): BeaconConfig['network'] => {
+  return Object.entries(CHAIN_CONFIGS).reduce((acc, [chainId, config]) => {
+    const networkName =
+      Object.keys(config).find(key =>
+        key.toLowerCase().includes(chainId.toString())
+      ) || chainId;
+
+    return {
+      ...acc,
+      [networkName]: {
+        address: config.beaconAddresses,
+        startBlock: config.beaconStartBlock,
+      },
+    };
+  }, {});
+};
 
 export default createConfig({
-  networks: {
-    baseSepolia: {
-      chainId: baseSepolia.id,
-      transport: http(process.env.BASE_SEPOLIA_RPC_URL),
-    },
-    optimismSepolia: {
-      chainId: optimismSepolia.id,
-      transport: http(process.env.OPTIMISM_SEPOLIA_RPC_URL),
-    },
-    arbitrumSepolia: {
-      chainId: arbitrumSepolia.id,
-      transport: http(process.env.ARBITRUM_SEPOLIA_RPC_URL),
-    },
-  },
+  networks: createNetworkConfigs(),
   blocks: {
     CurrentPriceUpdate: {
-      network: {
-        baseSepolia: {
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-          interval: Math.round(HOUR / CHAIN_CONFIG[baseSepolia.id].blockTime),
-        },
-        optimismSepolia: {
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            HOUR / CHAIN_CONFIG[optimismSepolia.id].blockTime
-          ),
-        },
-        arbitrumSepolia: {
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            HOUR / CHAIN_CONFIG[arbitrumSepolia.id].blockTime
-          ),
-        },
-      },
+      network: createBlockConfigs('HOUR'),
     },
     PriceSnapshotUpdate: {
-      network: {
-        baseSepolia: {
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-          interval: Math.round(DAY / CHAIN_CONFIG[baseSepolia.id].blockTime),
-        },
-        optimismSepolia: {
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            DAY / CHAIN_CONFIG[optimismSepolia.id].blockTime
-          ),
-        },
-        arbitrumSepolia: {
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            DAY / CHAIN_CONFIG[arbitrumSepolia.id].blockTime
-          ),
-        },
-      },
+      network: createBlockConfigs('DAY'),
     },
     APRSnapshotUpdate: {
-      network: {
-        baseSepolia: {
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-          interval: Math.round(DAY / CHAIN_CONFIG[baseSepolia.id].blockTime),
-        },
-        optimismSepolia: {
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            DAY / CHAIN_CONFIG[optimismSepolia.id].blockTime
-          ),
-        },
-        arbitrumSepolia: {
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-          interval: Math.round(
-            DAY / CHAIN_CONFIG[arbitrumSepolia.id].blockTime
-          ),
-        },
-      },
+      network: createBlockConfigs('DAY'),
     },
   },
   contracts: {
     Factory: {
-      network: {
-        baseSepolia: {
-          address: CHAIN_CONFIG[baseSepolia.id].factoryAddress,
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-        },
-        optimismSepolia: {
-          address: CHAIN_CONFIG[optimismSepolia.id].factoryAddress,
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-        },
-        arbitrumSepolia: {
-          address: CHAIN_CONFIG[arbitrumSepolia.id].factoryAddress,
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-        },
-      },
+      network: createFactoryConfig(),
       abi: FactoryAbi,
     },
     RiskEngine: {
-      network: {
-        baseSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[baseSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event ProtocolDeployed(uint256 indexed protocolId, address indexed riskEngine, address indexed timelock, address initialGovernor)'
-            ),
-            parameter: 'riskEngine',
-          }),
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-        },
-        optimismSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[optimismSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event ProtocolDeployed(uint256 indexed protocolId, address indexed riskEngine, address indexed timelock, address initialGovernor)'
-            ),
-            parameter: 'riskEngine',
-          }),
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-        },
-        arbitrumSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[arbitrumSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event ProtocolDeployed(uint256 indexed protocolId, address indexed riskEngine, address indexed timelock, address initialGovernor)'
-            ),
-            parameter: 'riskEngine',
-          }),
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-        },
-      },
+      network: createRiskEngineConfig(),
       abi: RiskEngineAbi,
     },
     PToken: {
-      network: {
-        baseSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[baseSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event PTokenDeployed(uint256 indexed protocolId, uint256 indexed index, address indexed pToken, address timelock)'
-            ),
-            parameter: 'pToken',
-          }),
-          startBlock: CHAIN_CONFIG[baseSepolia.id].factoryStartBlock,
-        },
-        optimismSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[optimismSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event PTokenDeployed(uint256 indexed protocolId, uint256 indexed index, address indexed pToken, address timelock)'
-            ),
-            parameter: 'pToken',
-          }),
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].factoryStartBlock,
-        },
-        arbitrumSepolia: {
-          address: factory({
-            address: CHAIN_CONFIG[arbitrumSepolia.id].factoryAddress,
-            event: parseAbiItem(
-              'event PTokenDeployed(uint256 indexed protocolId, uint256 indexed index, address indexed pToken, address timelock)'
-            ),
-            parameter: 'pToken',
-          }),
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].factoryStartBlock,
-        },
-      },
+      network: createPTokenConfig(),
       abi: PTokenAbi,
     },
     Beacon: {
-      network: {
-        baseSepolia: {
-          address: CHAIN_CONFIG[baseSepolia.id].beaconAddresses,
-          startBlock: CHAIN_CONFIG[baseSepolia.id].beaconStartBlock,
-        },
-        optimismSepolia: {
-          address: CHAIN_CONFIG[optimismSepolia.id].beaconAddresses,
-          startBlock: CHAIN_CONFIG[optimismSepolia.id].beaconStartBlock,
-        },
-        arbitrumSepolia: {
-          address: CHAIN_CONFIG[arbitrumSepolia.id].beaconAddresses,
-          startBlock: CHAIN_CONFIG[arbitrumSepolia.id].beaconStartBlock,
-        },
-      },
+      network: createBeaconConfig(),
       abi: BeaconAbi,
     },
   },
