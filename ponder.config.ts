6efebfc52e446b0ef01bc3704b7f476fe412e18a
Pedro Yves Fracari <yves.fracari@gmail.com>
2025-03-06 12:49:35 -0600

    fix: add comment of monad chain

--- ponder.config.ts
+++ ponder.config.ts
@@ -43,6 +43,13 @@ const CHAIN_CONFIGS: Record<ChainId, ChainConfig> = {
     blockTime: 0.2,
     rpcEnvKeys: ['ARBITRUM_SEPOLIA_RPC_URL'],
   },
+  // [monadTestnet.id]: {
+  //   chainId: monadTestnet.id,
+  //   factoryAddress: '0x0e2ef7AEEef695F9c8D463ce31561B43EC14e453',
+  //   factoryStartBlock: 2895130,
+  //   blockTime: 0.5,
+  //   rpcEnvKeys: ['MONAD_TESTNET_RPC_URL_1', 'MONAD_TESTNET_RPC_URL_2'],
+  // },
   // TODO: PIKE-124
   // [hyperliquidTestnet.id]: {
   //   chainId: hyperliquidTestnet.id,
